906ee737e97bf234ac0dad6219fd79056113b131
vangtt <giangthanhvan12@gmail.com>
2023-02-28 10:55:38 +0700

    TH1

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Input number a");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input number a");
+//        int a = scanner.nextInt();
+//        System.out.println("Input number b");
+//        int b = scanner.nextInt();
+//        System.out.println("Input number c");
+//        int c = scanner.nextInt();
+
+//        if (a > b) {
+//            if (a > c) {
+//                System.out.println("Max is " + a);
+//            } else {
+//                System.out.println("Max is " + c);
+//            }
+//        } else {
+//            if (b > c) {
+//                System.out.println("Max is " + b);
+//            } else {
+//                System.out.println("Max is " + c);
+//            }
+//        }
+//        int max = 0;
+//        max = a>b?((a>c)?a:c):((b>c?b:c));
+//        System.out.println("Max is " + max);
+        Date date = new Date();
+        System.out.println("Time now" + date);
+    }
+}
